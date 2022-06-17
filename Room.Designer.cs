f407c8f11349b11d32acc7a90de3a91dd93e1cfa
matthew28302 <86336241+matthew28302@users.noreply.github.com>
2022-06-17 23:30:49 +0700

    Update Room.Designer.cs

--- Room.Designer.cs
+++ Room.Designer.cs
@@ -1,6 +1,6 @@
 namespace DrawEveything
 {
-    partial class Room
+    partial class Register
     {
         /// <summary>
         /// Required designer variable.
@@ -28,84 +28,142 @@ namespace DrawEveything
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRoom1 = new System.Windows.Forms.Button();
-            this.btnRoom2 = new System.Windows.Forms.Button();
-            this.lbRoom1 = new System.Windows.Forms.Label();
-            this.lbRoom2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tbRetypePassword = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btRegister = new System.Windows.Forms.Button();
+            this.cbShowPassword = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnRoom1
+            // tbRetypePassword
             // 
-            this.btnRoom1.BackColor = System.Drawing.Color.Lime;
-            this.btnRoom1.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRoom1.Location = new System.Drawing.Point(146, 137);
-            this.btnRoom1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnRoom1.Name = "btnRoom1";
-            this.btnRoom1.Size = new System.Drawing.Size(143, 83);
-            this.btnRoom1.TabIndex = 0;
-            this.btnRoom1.Text = "Room 1";
-            this.btnRoom1.UseVisualStyleBackColor = false;
-            this.btnRoom1.Click += new System.EventHandler(this.btnRoom1_Click);
+            this.tbRetypePassword.Location = new System.Drawing.Point(276, 172);
+            this.tbRetypePassword.Name = "tbRetypePassword";
+            this.tbRetypePassword.Size = new System.Drawing.Size(275, 22);
+            this.tbRetypePassword.TabIndex = 29;
+            this.tbRetypePassword.UseSystemPasswordChar = true;
             // 
-            // btnRoom2
+            // tbPassword
             // 
-            this.btnRoom2.BackColor = System.Drawing.Color.Lime;
-            this.btnRoom2.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRoom2.Location = new System.Drawing.Point(411, 137);
-            this.btnRoom2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnRoom2.Name = "btnRoom2";
-            this.btnRoom2.Size = new System.Drawing.Size(136, 83);
-            this.btnRoom2.TabIndex = 1;
-            this.btnRoom2.Text = "Room 2";
-            this.btnRoom2.UseVisualStyleBackColor = false;
-            this.btnRoom2.Click += new System.EventHandler(this.btnRoom2_Click);
+            this.tbPassword.Location = new System.Drawing.Point(276, 211);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(275, 22);
+            this.tbPassword.TabIndex = 28;
+            this.tbPassword.UseSystemPasswordChar = true;
             // 
-            // lbRoom1
+            // tbUserName
             // 
-            this.lbRoom1.AutoSize = true;
-            this.lbRoom1.Location = new System.Drawing.Point(171, 230);
-            this.lbRoom1.Name = "lbRoom1";
-            this.lbRoom1.Size = new System.Drawing.Size(70, 16);
-            this.lbRoom1.TabIndex = 2;
-            this.lbRoom1.Text = "Có 0 người";
+            this.tbUserName.Location = new System.Drawing.Point(276, 132);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(275, 22);
+            this.tbUserName.TabIndex = 27;
             // 
-            // lbRoom2
+            // btExit
             // 
-            this.lbRoom2.AutoSize = true;
-            this.lbRoom2.Location = new System.Drawing.Point(448, 230);
-            this.lbRoom2.Name = "lbRoom2";
-            this.lbRoom2.Size = new System.Drawing.Size(70, 16);
-            this.lbRoom2.TabIndex = 3;
-            this.lbRoom2.Text = "Có 0 người";
+            this.btExit.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btExit.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.btExit.FlatAppearance.BorderSize = 2;
+            this.btExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btExit.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExit.Location = new System.Drawing.Point(376, 279);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(108, 46);
+            this.btExit.TabIndex = 26;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = false;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
             // 
-            // pictureBox2
+            // btRegister
             // 
-            this.pictureBox2.Image = global::DrawEveything.Properties.Resources.Ảnh1;
-            this.pictureBox2.Location = new System.Drawing.Point(233, 26);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(261, 91);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 21;
-            this.pictureBox2.TabStop = false;
+            this.btRegister.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btRegister.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.btRegister.FlatAppearance.BorderSize = 2;
+            this.btRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btRegister.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRegister.Location = new System.Drawing.Point(127, 279);
+            this.btRegister.Name = "btRegister";
+            this.btRegister.Size = new System.Drawing.Size(119, 46);
+            this.btRegister.TabIndex = 25;
+            this.btRegister.Text = "Register";
+            this.btRegister.UseVisualStyleBackColor = false;
+            this.btRegister.Click += new System.EventHandler(this.btRegister_Click);
             // 
-            // Room
+            // cbShowPassword
+            // 
+            this.cbShowPassword.AutoSize = true;
+            this.cbShowPassword.Location = new System.Drawing.Point(424, 239);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(127, 20);
+            this.cbShowPassword.TabIndex = 24;
+            this.cbShowPassword.Text = "Show password!";
+            this.cbShowPassword.UseVisualStyleBackColor = true;
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 22);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Confirm password: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 22);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Password: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 22);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "User name: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 38);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "REGISTER";
+            // 
+            // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(711, 360);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.lbRoom2);
-            this.Controls.Add(this.lbRoom1);
-            this.Controls.Add(this.btnRoom2);
-            this.Controls.Add(this.btnRoom1);
+            this.ClientSize = new System.Drawing.Size(620, 360);
+            this.Controls.Add(this.tbRetypePassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btRegister);
+            this.Controls.Add(this.cbShowPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Room";
-            this.Text = "Room";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Name = "Register";
+            this.Text = "Register";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,10 +171,15 @@ namespace DrawEveything
 
         #endregion
 
-        private System.Windows.Forms.Button btnRoom1;
-        private System.Windows.Forms.Button btnRoom2;
-        private System.Windows.Forms.Label lbRoom1;
-        private System.Windows.Forms.Label lbRoom2;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tbRetypePassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btRegister;
+        private System.Windows.Forms.CheckBox cbShowPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
